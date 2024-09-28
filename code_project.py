d598cb218f23d813b01711196c3ec1d2536bb20d
Hai <haint.vietnam@gmail.com>
2024-09-28 20:53:15 +0700

    Update code buoi 8

--- code_project.py
+++ code_project.py
@@ -29,39 +29,47 @@ class Login(QtWidgets.QMainWindow):
 class Main(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi("test_mindx.ui", self)
-        self.icon_stn.clicked.connect(self.sach_thieu_nhi)
-        self.btn_opt_stn.clicked.connect(self.sach_thieu_nhi)
-        self.icon_stk.clicked.connect(self.sach_tham_khao)
-        self.btn_opt_stk.clicked.connect(self.sach_tham_khao)
-        self.icon_skt.clicked.connect(self.sach_kinh_te) 
-        self.btn_opt_skt.clicked.connect(self.sach_kinh_te)
-        self.icon_svh.clicked.connect(self.sach_van_hoc)
-        self.btn_opt_svh.clicked.connect(self.sach_van_hoc)      
-    def sach_thieu_nhi(self):
-        sach_thieu_nhi.show()
-    def sach_tham_khao(self):
-        sachthamkhao.show()
-    def sach_kinh_te(self):
-        sachkinhte.show()
-    def sach_van_hoc(self):
-        sach_van_hoc.show()
+        uic.loadUi("Home.ui", self)
+        self.icon_pha_do_1.clicked.connect(self.pha_do_1)
+        self.icon_pha_do_2.clicked.connect(self.pha_do_2)
+        self.icon_cong_duc_1.clicked.connect(self.cong_duc_1)
+        self.icon_cong_duc_2.clicked.connect(self.cong_duc_2)
+        self.icon_pha_he_1.clicked.connect(self.pha_he_1) 
+        self.icon_pha_he_2.clicked.connect(self.pha_he_2)
+        self.icon_pha_ky_1.clicked.connect(self.pha_ky_1)
+        self.icon_pha_ky_2.clicked.connect(self.pha_ky_2)      
+    def pha_do_1(self):
+        pha_do_1.show()
+    def pha_do_2(self):
+        pha_do_2.show()
+    def cong_duc_1(self):
+        cong_duc_1.show()
+    def cong_duc_2(self):
+        cong_duc_2.show()
+    def pha_he_1(self):
+        pha_he_1.show()
+    def pha_he_2(self):
+        pha_he_2.show()
+    def pha_ky_1(self):
+        pha_he_2.show()
+    def pha_ky_2(self):
+        pha_he_2.show()
         
-class SachThieuNhi(QtWidgets.QMainWindow):
+class ph1(QtWidgets.QMainWindow):
     def __init__(self):
-        super(SachThieuNhi,self).__init__()
-        pagename = "sach_thieu_nhi.ui"
+        super(ph1,self).__init__()
+        pagename = ".ui"
         uic. loadUi (pagename, self)
         self.btn_back.clicked.connect(self.close)
         self.btn_close.clicked.connect(self.close)
         
-    def sach_thieu_nhi(self):
-        sach_thieu_nhi.show()    
+    def p_h_1(self):
+        pha_he_1.show()    
         mainPage.close()
         self.close()
-class SachThamKhao(QtWidgets.QMainWindow):
+class ph2(QtWidgets.QMainWindow):
     def __init__(self):
-        super(SachThamKhao,self).__init__()
+        super(ph2,self).__init__()
         pagename = "sach_thieu_nhi(1).ui"
         uic. loadUi (pagename, self)
         self.btn_back.clicked.connect(self.close)
@@ -89,13 +97,21 @@ if __name__ == '__main__':
     loginPage = Login()
     loginPage.show()
     mainPage = Main()
-    sach_thieu_nhi = SachThieuNhi()
-    sachthamkhao = SachThamKhao()
-    sachkinhte = SachKinhTe()
-    sach_van_hoc = SachVanHoc()
+    pha_do_1 = pd1()
+    pha_do_2 = pd2()
+    pha_he_1 = ph1()
+    pha_he_2 = ph2()
+    pha_ky_1 = pk1()
+    pha_ky_2 = pk2()
+    cong_duc_1 = cd1()
+    cong_duc_2 = cd2()
     # Thiết lập hộp thoại thông báo lỗi
     msg_box = QMessageBox()
     msg_box.setWindowTitle("Lỗi")
     msg_box.setIcon(QMessageBox.Icon.Warning)
     msg_box.setStyleSheet("background-color: #F8F2EC; color: #356a9c")
     app.exec()
+
+
+
+
