c8af01f91e4b49e65c655839fd29c576e314b64d
Hai <haint.vietnam@gmail.com>
2024-10-05 21:07:13 +0700

    sua_giao_dien_pha_do_dong_ho

--- code_project.py
+++ code_project.py
@@ -6,8 +6,8 @@ import sys
 class Login(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi("SigninScreen.ui", self)
-        self.Signin_button.clicked.connect(self.check_login)
+        uic.loadUi("pha_do_dong_ho.ui", self)
+        # self.Signin_button.clicked.connect(self.check_login)
         # self.btn_register.clicked.connect(self.showRegister)
     def check_login(self):
         Username = self.txtUsername.text()
@@ -29,89 +29,88 @@ class Login(QtWidgets.QMainWindow):
 class Main(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi("Home.ui", self)
-        self.icon_pha_do_1.clicked.connect(self.pha_do_1)
-        self.icon_pha_do_2.clicked.connect(self.pha_do_2)
-        self.icon_cong_duc_1.clicked.connect(self.cong_duc_1)
-        self.icon_cong_duc_2.clicked.connect(self.cong_duc_2)
-        self.icon_pha_he_1.clicked.connect(self.pha_he_1) 
-        self.icon_pha_he_2.clicked.connect(self.pha_he_2)
-        self.icon_pha_ky_1.clicked.connect(self.pha_ky_1)
-        self.icon_pha_ky_2.clicked.connect(self.pha_ky_2)      
-    def pha_do_1(self):
-        pha_do_1.show()
-    def pha_do_2(self):
-        pha_do_2.show()
-    def cong_duc_1(self):
-        cong_duc_1.show()
-    def cong_duc_2(self):
-        cong_duc_2.show()
-    def pha_he_1(self):
-        pha_he_1.show()
-    def pha_he_2(self):
-        pha_he_2.show()
-    def pha_ky_1(self):
-        pha_he_2.show()
-    def pha_ky_2(self):
-        pha_he_2.show()
+        uic.loadUi("pha_do_dong_ho.ui", self)
+#         self.icon_pha_do_1.clicked.connect(self.pha_do_1)
+#         self.icon_pha_do_2.clicked.connect(self.pha_do_2)
+#         self.icon_cong_duc_1.clicked.connect(self.cong_duc_1)
+#         self.icon_cong_duc_2.clicked.connect(self.cong_duc_2)
+#         self.icon_pha_he_1.clicked.connect(self.pha_he_1) 
+#         self.icon_pha_he_2.clicked.connect(self.pha_he_2)
+#         self.icon_pha_ky_1.clicked.connect(self.pha_ky_1)
+#         self.icon_pha_ky_2.clicked.connect(self.pha_ky_2)      
+#     def pha_do_1(self):
+#         pha_do_1.show()
+#     def pha_do_2(self):
+#         pha_do_2.show()
+#     def cong_duc_1(self):
+#         cong_duc_1.show()
+#     def cong_duc_2(self):
+#         cong_duc_2.show()
+#     def pha_he_1(self):
+#         pha_he_1.show()
+#     def pha_he_2(self):
+#         pha_he_2.show()
+#     def pha_ky_1(self):
+#         pha_he_2.show()
+#     def pha_ky_2(self):
+#         pha_he_2.show()
         
-class ph1(QtWidgets.QMainWindow):
-    def __init__(self):
-        super(ph1,self).__init__()
-        pagename = ".ui"
-        uic. loadUi (pagename, self)
-        self.btn_back.clicked.connect(self.close)
-        self.btn_close.clicked.connect(self.close)
+# class ph1(QtWidgets.QMainWindow):
+#     def __init__(self):
+#         super(ph1,self).__init__()
+#         pagename = ".ui"
+#         uic. loadUi (pagename, self)
+#         self.btn_back.clicked.connect(self.close)
+#         self.btn_close.clicked.connect(self.close)
         
-    def p_h_1(self):
-        pha_he_1.show()    
-        mainPage.close()
-        self.close()
-class ph2(QtWidgets.QMainWindow):
-    def __init__(self):
-        super(ph2,self).__init__()
-        pagename = "sach_thieu_nhi(1).ui"
-        uic. loadUi (pagename, self)
-        self.btn_back.clicked.connect(self.close)
-        self.btn_close.clicked.connect(self.close)
-class SachKinhTe(QtWidgets.QMainWindow):
-    def __init__(self):
-        super(SachKinhTe,self).__init__()
-        pagename = "sach_kinh_te.ui"
-        uic. loadUi (pagename, self)
-        self.btn_back.clicked.connect(self.close)
-        self.btn_close.clicked.connect(self.close)
-class SachVanHoc(QtWidgets.QMainWindow):
-    def __init__(self):
-        super(SachVanHoc,self).__init__()
-        pagename = "sach_van_hoc(1).ui"
-        uic.loadUi (pagename, self)
-        self.btn_back.clicked.connect(self.close)
-        self.btn_close.clicked.connect(self.close)
+#     def p_h_1(self):
+#         pha_he_1.show()    
+#         mainPage.close()
+#         self.close()
+# class ph2(QtWidgets.QMainWindow):
+#     def __init__(self):
+#         super(ph2,self).__init__()
+#         pagename = "sach_thieu_nhi(1).ui"
+#         uic. loadUi (pagename, self)
+#         self.btn_back.clicked.connect(self.close)
+#         self.btn_close.clicked.connect(self.close)
+# class SachKinhTe(QtWidgets.QMainWindow):
+#     def __init__(self):
+#         super(SachKinhTe,self).__init__()
+#         pagename = "sach_kinh_te.ui"
+#         uic. loadUi (pagename, self)
+#         self.btn_back.clicked.connect(self.close)
+#         self.btn_close.clicked.connect(self.close)
+# class SachVanHoc(QtWidgets.QMainWindow):
+#     def __init__(self):
+#         super(SachVanHoc,self).__init__()
+#         pagename = "sach_van_hoc(1).ui"
+#         uic.loadUi (pagename, self)
+#         self.btn_back.clicked.connect(self.close)
+#         self.btn_close.clicked.connect(self.close)
 
 
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
-    #Tạo các đối tượng tương ứng với các trang giao diện
+#     #Tạo các đối tượng tương ứng với các trang giao diện
     loginPage = Login()
     loginPage.show()
     mainPage = Main()
-    pha_do_1 = pd1()
-    pha_do_2 = pd2()
-    pha_he_1 = ph1()
-    pha_he_2 = ph2()
-    pha_ky_1 = pk1()
-    pha_ky_2 = pk2()
-    cong_duc_1 = cd1()
-    cong_duc_2 = cd2()
+#     pha_do_1 = pd1()
+#     pha_do_2 = pd2()
+#     pha_he_1 = ph1()
+#     pha_he_2 = ph2()
+#     pha_ky_1 = pk1()
+#     pha_ky_2 = pk2()
+#     cong_duc_1 = cd1()
+#     cong_duc_2 = cd2()
     # Thiết lập hộp thoại thông báo lỗi
     msg_box = QMessageBox()
     msg_box.setWindowTitle("Lỗi")
     msg_box.setIcon(QMessageBox.Icon.Warning)
     msg_box.setStyleSheet("background-color: #F8F2EC; color: #356a9c")
     app.exec()
-
 
 
 
